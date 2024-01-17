d17797f73ad2fb6c18388bdd8cdc709a932f6461
prafpawar11 <prafpawar11@gmail.com>
2024-01-17 08:17:48 +0530

    BDD Framework is created

---
+++ src/test/java/TestLayer/LoginPageTest.java
@@ -0,0 +1,11 @@
+package TestLayer;
+
+import PageLayer.LoginPage;
+
+public class LoginPageTest {
+
+	public static void main(String[] args) {
+		LoginPage login = new LoginPage();
+		login.loginFunctionality();
+	}
+}
